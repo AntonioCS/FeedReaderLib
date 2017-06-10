8366881c5e9d5a8329a1783818f32cfa3c2cbbc4
AntonioCS <antoniocs@gmail.com>
2017-06-10 22:02:39 +0100

    Node checker code

--- src/Feeds/RssFeed.h
+++ src/Feeds/RssFeed.h
@@ -6,25 +6,25 @@
 #include <string>
 #include <vector>
 #include <algorithm> //find
-#include <iostream> //using it for debuing will remove in the future
+#include <iostream> //using it for debugging will remove in the future
 #include "../XMLNode.h"
 
 namespace AcsFeedReader {
 
-        /*
-        enum class RssAttribute {
-            title,
-            description,
-            link,
-            category,
-            copyright,
-            language,
-            lastBuildDate,
-            managingEditor,
-            pubDate,
-            webMaster,
-            generator
-        };
+    /*
+    enum class RssAttribute {
+        title,
+        description,
+        link,
+        category,
+        copyright,
+        language,
+        lastBuildDate,
+        managingEditor,
+        pubDate,
+        webMaster,
+        generator
+    };
      */
     struct RssFeedImage {
         std::string url;
@@ -41,7 +41,7 @@ namespace AcsFeedReader {
         std::string link;
         std::string category;
         std::string comments;
-        std::string pubDate; //Possible change to some date format
+        std::string pubDate;
     };
 
     class RssFeed {
@@ -50,11 +50,20 @@ namespace AcsFeedReader {
         RssFeed(XMLNode channel);
         ~RssFeed();
 
-        std::string getAttr(const char *);
-        std::string getAttr(const std::string);
+        std::string getAttr(const char *) const noexcept;
+        std::string getAttr(const std::string) const noexcept;
+
+        int itemTotal() const noexcept {
+            return static_cast<int>(items.size());
+        }
+        
+        RssFeedItem getItem(int n) const {
+            //return items.at(static_cast<std::vector<::size_t>(n));
+            return RssFeedItem{items.at(n)};
+        }
 
     private:
-        const std::array<std::string, 11> attrs{
+        const std::array<std::string, 12> attrs{
             std::string{"title"},
             std::string{"description"},
             std::string{"link"},
@@ -65,8 +74,8 @@ namespace AcsFeedReader {
             std::string{"managingEditor"},
             std::string{"pubDate"},
             std::string{"webMaster"},
-            std::string{"generator"}
-        };
+            std::string{"generator"},
+            std::string{"docs"}};
 
         std::unordered_map<std::string, std::string> attrsValues;
         RssFeedImage image;
@@ -74,6 +83,11 @@ namespace AcsFeedReader {
 
         void setAttr(const char *, std::string);
         void setAttr(const std::string, std::string);
+
+        void addItem(XMLNode &);
+        void setImage(XMLNode &);
+
+        RssFeedItem generateItem(XMLNode &) const noexcept;
     };
 
 }
